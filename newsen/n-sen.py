9bdece1cbe717cad3e6389af2d81a66ba634c7ec
yuyuyu1234537842039824098 <38784894+yuyuyu1234537842039824098@users.noreply.github.com>
2018-05-02 17:08:19 +0800

    Add files via upload

--- newsen/n-sen.py
+++ newsen/n-sen.py
@@ -8,7 +8,7 @@ couch = couchdb.Server(COUCHDB_ADDRESS)
 db = couch[COUCHDB_TWEETS_DBNAME]
 counter = 0
 while True:
-  mango = {'selector':{'_id':{"$gt":None}}, "sort":[{"_id":"asc}], "limit":1000, "skip": counter*1000}
+  mango = {'selector':{'_id':{"$gt":None}}, "sort":[{"_id":"asc"}], "limit":1000, "skip": counter*1000}
   results = list(db.find(mango))
   if len(results) == 0:
       break
