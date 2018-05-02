3858db1ea3250251c35c0400bc21e3d40110c54a
yuyuyu1234537842039824098 <38784894+yuyuyu1234537842039824098@users.noreply.github.com>
2018-05-02 17:00:06 +0800

    Add files via upload

--- newsen/n-sen.py
+++ newsen/n-sen.py
@@ -1,18 +1,18 @@
-import geopandas as gpd
-from shapely.geometry import Point, Polygon
 import couchdb
 import json
 import sentiment_mod as sentiment
-COUCHDB_ADDRESS = 'http://admin:admin@localhost:5984/'
+COUCHDB_ADDRESS = 'http://admin:admin@115.146.95.21:5984/'
 COUCHDB_TWEETS_DBNAME = 'twitter'
 couch = couchdb.Server()
 couch = couchdb.Server(COUCHDB_ADDRESS)
 db = couch[COUCHDB_TWEETS_DBNAME]
+counter = 0
 while True:
   mango = {'selector':{'_id':{"$gt":None}}, "sort":[{"_id":"asc}], "limit":1000, "skip": counter*1000}
   results = list(db.find(mango))
   if len(results) == 0:
       break
+  counter += 1                                                   
   for result in results:
       tweet_id = result['_id']
       tweet = result['text']                                                 
